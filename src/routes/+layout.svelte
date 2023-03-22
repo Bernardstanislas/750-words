1f9c8757e9d6a319d6e5c6e18a28974c957ae910
Stanislas Bernard <bernardstanislas@gmail.com>
2023-03-22 21:14:08 +0100

    :sparkles: Store user input in local storage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,19 @@
 </script>
 
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { setContext } from 'svelte';
 	import { writable } from 'svelte/store';
 	import WordCounter from '../lib/word-counter.svelte';
 
-	const journal = writable('');
+	const storedJournal = browser ? window.localStorage.getItem(JOURNAL) : '';
+	const journal = writable(storedJournal || '');
+	journal.subscribe((value) => {
+		if (browser) {
+			localStorage.setItem(JOURNAL, value);
+		}
+	});
+
 	setContext(JOURNAL, journal);
 </script>
 
