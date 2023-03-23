ee6e6e18e7d2bef5deb13b3dad3e3a6fdcb69b21
Stanislas Bernard <bernardstanislas@gmail.com>
2023-03-23 19:28:36 +0100

    :green_heart: Increase unit tests timeouts

--- src/lib/crypto.test.ts
+++ src/lib/crypto.test.ts
@@ -7,38 +7,42 @@ import {
 } from '$lib/crypto';
 import { describe, it, expect } from 'vitest';
 
-describe('The crypto module', () => {
-	const content = 'La barbe de la femme à Georges Moustaki';
+describe(
+	'The crypto module',
+	() => {
+		const content = 'La barbe de la femme à Georges Moustaki';
 
-	it('generates keypairs', async () => {
-		const keyPair = await generateKeyPair();
-		expect(keyPair.privateKey).toBeDefined();
-		expect(keyPair.publicKey).toBeDefined();
-	});
+		it('generates keypairs', async () => {
+			const keyPair = await generateKeyPair();
+			expect(keyPair.privateKey).toBeDefined();
+			expect(keyPair.publicKey).toBeDefined();
+		});
 
-	it('encrypts content that can be decrypted', async () => {
-		const keyPair = await generateKeyPair();
+		it('encrypts content that can be decrypted', async () => {
+			const keyPair = await generateKeyPair();
 
-		const encryptedContent = await encryptContent(content, keyPair.publicKey);
+			const encryptedContent = await encryptContent(content, keyPair.publicKey);
 
-		expect(encryptedContent).toBeDefined();
+			expect(encryptedContent).toBeDefined();
 
-		const decryptedContent = await decryptContent(encryptedContent, keyPair.privateKey);
+			const decryptedContent = await decryptContent(encryptedContent, keyPair.privateKey);
 
-		expect(decryptedContent).toBe(content);
-	});
+			expect(decryptedContent).toBe(content);
+		});
 
-	it('exports and imports keypairs', async () => {
-		const keyPair = await generateKeyPair();
+		it('exports and imports keypairs', async () => {
+			const keyPair = await generateKeyPair();
 
-		const exportedKeyPair = await keyPairToString(keyPair);
-		expect(exportedKeyPair).toBeDefined();
+			const exportedKeyPair = await keyPairToString(keyPair);
+			expect(exportedKeyPair).toBeDefined();
 
-		const importedKeyPair = await stringToKeyPair(exportedKeyPair);
-		expect(importedKeyPair).toBeDefined();
+			const importedKeyPair = await stringToKeyPair(exportedKeyPair);
+			expect(importedKeyPair).toBeDefined();
 
-		const encryptedContent = await encryptContent(content, keyPair.publicKey);
-		const decryptedContent = await decryptContent(encryptedContent, importedKeyPair.privateKey);
-		expect(decryptedContent).toBe(content);
-	});
-});
+			const encryptedContent = await encryptContent(content, keyPair.publicKey);
+			const decryptedContent = await decryptContent(encryptedContent, importedKeyPair.privateKey);
+			expect(decryptedContent).toBe(content);
+		});
+	},
+	{ timeout: 10000 }
+);
