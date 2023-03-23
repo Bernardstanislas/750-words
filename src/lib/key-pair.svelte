ca83e5aa0013460fa47c9fe0825eaf84ef815076
Sammy Teillet <sammy.teillet@gmail.com>
2023-03-23 18:53:49 +0100

    :sparkles: Add key-pair component

---
+++ src/lib/key-pair.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { generateKeyPair, keyPairToString, stringToKeyPair } from '../lib/crypto';
+
+	let localKeyPair: CryptoKeyPair | null;
+
+	const init = async () => {
+		const storedKeyPair = browser ? window.localStorage.getItem('keyPair') : null;
+		localKeyPair = storedKeyPair ? await stringToKeyPair(storedKeyPair) : null;
+		return localKeyPair;
+	};
+
+	init();
+
+	const generateAndStoreKey = async () => {
+		const newKeyPair = await generateKeyPair();
+		localKeyPair = newKeyPair;
+		const newKeyPairString = await keyPairToString(newKeyPair);
+		window.localStorage.setItem('keyPair', newKeyPairString);
+	};
+</script>
+
+{#if localKeyPair}
+	<p>You have a key pair! 🎉</p>
+{:else}
+	<button on:click={generateAndStoreKey}>Give me the key</button>
+{/if}
