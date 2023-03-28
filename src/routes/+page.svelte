4b7378e3ac53cec546bbf4cfbb6c85b78be76fd5
Stanislas Bernard <bernardstanislas@gmail.com>
2023-03-28 10:49:01 +0200

    :sparkles: Set encrypted journal content in hidden input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	import { journal } from '$lib/stores/journal';
+	import { journal, encryptedJournal } from '$lib/stores/journal';
 	import KeyPair from '../lib/key-pair.svelte';
-
-	$: encryptedJournal = $journal.length;
 </script>
 
 <h1>750 words</h1>
@@ -12,6 +10,6 @@
 		Today's journal
 		<textarea data-testid="journal" bind:value={$journal} />
 	</label>
-	<input name="encrypted_journal" hidden value={encryptedJournal} />
+	<input name="encrypted_journal" hidden value={$encryptedJournal.value} />
 	<button type="submit">Submit</button>
 </form>
