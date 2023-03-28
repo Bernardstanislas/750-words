d78753a9b125fc63bd5a1327b3e2aaa767cdb70b
Stanislas Bernard <bernardstanislas@gmail.com>
2023-03-28 15:55:50 +0200

    :recycle: Send base64 to server

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { journal, encryptedJournal } from '$lib/stores/journal';
 	import KeyPair from '../lib/key-pair.svelte';
+
+	$: base64EncodedJournal = browser
+		? window.btoa(
+				String.fromCharCode(...new Uint8Array($encryptedJournal.value || new ArrayBuffer(0)))
+		  )
+		: '';
 </script>
 
 <h1>750 words</h1>
@@ -10,6 +17,6 @@
 		Today's journal
 		<textarea data-testid="journal" bind:value={$journal} />
 	</label>
-	<input name="encrypted_journal" hidden value={$encryptedJournal.value} />
+	<input name="encrypted_journal" hidden value={base64EncodedJournal} />
 	<button type="submit">Submit</button>
 </form>
