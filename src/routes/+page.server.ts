df002b703e5645b434c2be3d30406ed9d8a55264
Stanislas Bernard <bernardstanislas@gmail.com>
2023-03-30 18:26:23 +0200

    :sparkles: Store encrypted journal in mongodb

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,19 @@
 import type { Actions } from './$types';
+import { MongoClient } from 'mongodb';
+
+const uri = import.meta.env.VITE_MONGODB_URI;
+
+const client = new MongoClient(uri);
 
 export const actions = {
 	default: async ({ request }) => {
-		console.log('formData', await request.formData());
+		const formData = await request.formData();
+		const database = client.db('750words');
+		const journals = database.collection('journals');
+		await journals.updateOne(
+			{},
+			{ $set: { content: formData.get('encrypted_journal') } },
+			{ upsert: true }
+		);
 	}
 } satisfies Actions;
