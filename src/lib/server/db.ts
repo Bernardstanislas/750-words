c36717e70ec5cfb2ea3391de7122f264b1026a4e
Stanislas Bernard <bernardstanislas@gmail.com>
2023-03-30 18:49:57 +0200

    :sparkles: Db can get the content

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,22 +2,46 @@ import { MongoClient } from 'mongodb';
 
 const uri = import.meta.env.VITE_MONGODB_URI;
 
-const client: Client =
-	import.meta.env.MODE === 'test'
-		? {
-				store: (content: string) => Promise.resolve()
-		  }
-		: {
-				store: async (content: string) => {
-					const mongoClient = new MongoClient(uri);
-					const database = mongoClient.db('750words');
-					const journals = database.collection('journals');
-					await journals.updateOne({}, { $set: { content } }, { upsert: true });
-				}
-		  };
+const getTestClient = (): Client => {
+	let storedContent: string;
+
+	return {
+		store: async (content: string) => {
+			storedContent = content;
+		},
+		get: async () => {
+			if (!storedContent) {
+				throw new Error('No content stored');
+			}
+			return storedContent;
+		}
+	};
+};
+
+const getProductionClient = (): Client => {
+	const mongoClient = new MongoClient(uri);
+	const database = mongoClient.db('750words');
+	const journals = database.collection('journals');
+
+	return {
+		store: async (content: string) => {
+			await journals.updateOne({}, { $set: { content } }, { upsert: true });
+		},
+		get: async () => {
+			const journal = await journals.findOne({});
+			if (!journal) {
+				throw new Error('No journal found');
+			}
+			return journal.content;
+		}
+	};
+};
+
+const client: Client = import.meta.env.MODE === 'test' ? getTestClient() : getProductionClient();
 
 export type Client = {
 	store: (content: string) => Promise<void>;
+	get: () => Promise<string>;
 };
 
 export default client;
