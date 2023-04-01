a411f7df15f01d52ca2572a3138865e39ac1f7de
Stanislas Bernard <bernardstanislas@gmail.com>
2023-04-01 15:30:47 +0200

    :sparkles: Store content per key id

--- src/lib/server/db.test.ts
+++ src/lib/server/db.test.ts
@@ -1,12 +1,14 @@
 import { describe, it, expect } from 'vitest';
 import client from './db';
+import type { KeyId } from '$lib/crypto';
 
 describe('The test db client', () => {
 	it('stores the content', async () => {
 		const content = 'La barbe de la femme à Georges Moustaki';
-		await client.store(content);
+		const keyId = 'lol123' as KeyId;
+		await client.store(keyId, content);
 
-		const storedContent = await client.get();
+		const storedContent = await client.get(keyId);
 		expect(storedContent).toBe(content);
 	});
 });
