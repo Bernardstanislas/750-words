a80d657e0fc36d6a957ddf4e7c0239b519c1230a
Stanislas Bernard <bernardstanislas@gmail.com>
2023-04-01 15:08:47 +0200

    :sparkles: Display the public key id

--- src/lib/stores/key-pair.ts
+++ src/lib/stores/key-pair.ts
@@ -1,21 +1,30 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 import { generateKeyPair, keyPairToString, stringToKeyPair } from '$lib/crypto';
+import { keyId } from '$lib/crypto';
+
+type KeyPair = {
+	publicKey: CryptoKey;
+	privateKey: CryptoKey;
+	id: string;
+};
 
 const keyPairStoreFactory = () => {
-	const { subscribe, set } = writable<CryptoKeyPair>();
+	const { subscribe, set } = writable<KeyPair>();
 
 	const init = async () => {
 		const storedKeyPair = localStorage.getItem('keyPair');
 		const localKeyPair = storedKeyPair ? await stringToKeyPair(storedKeyPair) : null;
 		if (localKeyPair) {
-			set(localKeyPair);
+			const id = await keyId(localKeyPair.publicKey);
+			set({ ...localKeyPair, id });
 		}
 	};
 
 	const generate = async () => {
 		const newKeyPair = await generateKeyPair();
-		set(newKeyPair);
+		const id = await keyId(newKeyPair.publicKey);
+		set({ ...newKeyPair, id });
 		if (browser) {
 			const newKeyPairString = await keyPairToString(newKeyPair);
 			localStorage.setItem('keyPair', newKeyPairString);
