1482e759e925078b96d68ab22d3cf207d3f963f5
Stanislas Bernard <bernardstanislas@gmail.com>
2023-04-03 23:15:23 +0200

    :sparkles: Auto submit form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
+	import { onDestroy, tick } from 'svelte';
 	import { browser } from '$app/environment';
 	import keyPair from '$lib/stores/key-pair';
 	import { arrayBufferToBase64 } from '$lib/array-buffer';
 	import { journal, encryptedJournal, initJournalFromEncryptedContent } from '$lib/stores/journal';
 	import type { PageData } from './$types';
+	import type { Unsubscriber } from 'svelte/store';
 
 	export let data: PageData;
+	let form: HTMLFormElement;
+	let unsubscribe: Unsubscriber;
+
+	const subscribeToEncryptedJournalChanges = () => {
+		unsubscribe = encryptedJournal.subscribe(async (value) => {
+			if (value.dirty === false && value.encrypting === false) {
+				await tick();
+				form.submit();
+			}
+		});
+	};
+
 	$: if ($keyPair && data.encryptedTodaysJournal) {
 		initJournalFromEncryptedContent(data.encryptedTodaysJournal, $keyPair.privateKey);
 	}
@@ -13,6 +27,12 @@
 	$: base64EncodedJournal = browser
 		? arrayBufferToBase64($encryptedJournal.value || new ArrayBuffer(0))
 		: '';
+
+	onDestroy(() => {
+		if (unsubscribe) {
+			unsubscribe();
+		}
+	});
 </script>
 
 <p>Archives</p>
@@ -28,10 +48,14 @@
 		</li>
 	{/each}
 </ul>
-<form method="POST">
+<form method="POST" bind:this={form}>
 	<label>
 		Today's journal
-		<textarea data-testid="journal" bind:value={$journal} />
+		<textarea
+			data-testid="journal"
+			bind:value={$journal}
+			on:input|once={subscribeToEncryptedJournalChanges}
+		/>
 	</label>
 	<input name="key_id" hidden value={$keyPair?.id} />
 	<input name="encrypted_journal" hidden value={base64EncodedJournal} />
