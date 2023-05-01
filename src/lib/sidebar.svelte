99713f5e5481a99c972ad99c9d9014e0ad7cd4e5
Stanislas Bernard <bernardstanislas@gmail.com>
2023-05-01 19:27:46 +0200

    :sparkles: Only display last week archives

--- src/lib/sidebar.svelte
+++ src/lib/sidebar.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import Archives from '$lib/archives.svelte';
 	import Profile from '$lib/profile.svelte';
-	export let dates: Date[] = [];
+  export let lastWeek: {date: Date, hasArchive: boolean}[] = [];
 </script>
 
 <div class="flex items-center pb-4">
 	<Profile />
 	<h1 class="font-bold ml-4 text-3xl">750 words</h1>
 </div>
-<Archives {dates} />
+<Archives {lastWeek} />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 
 <div class="sm:flex px-4 sm:px-0 mx-auto container mt-4 sm:mt-2">
 	<div class="sm:basis-1/4">
-		<Sidebar dates={data.journals} />
+		<Sidebar lastWeek={data.lastWeek} />
 	</div>
 	<div class="sm:grow pt-3">
 		<slot />
