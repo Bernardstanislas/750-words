5d12e4364a1f92a720182f22ae91e3087c87920d
Stanislas Bernard <bernardstanislas@gmail.com>
2023-05-01 19:39:29 +0200

    :sparkles: Display archive date

--- src/routes/[month]/[day]/[year]/+page.svelte
+++ src/routes/[month]/[day]/[year]/+page.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
+  import { page } from '$app/stores';
 	import type { PageData } from './$types';
 	import keyPair from '$lib/stores/key-pair';
 	import { initJournalFromEncryptedContent, journal } from '$lib/stores/journal';
+  import { getDateFromDayMonthYear } from '$lib/dates';
 
+	const date = getDateFromDayMonthYear($page.params.day, $page.params.month, $page.params.year);
 	export let data: PageData;
 	$: if ($keyPair && data.encryptedJournal) {
 		initJournalFromEncryptedContent(data.encryptedJournal, $keyPair.privateKey);
 	}
 </script>
 
-<div data-testid="journal">{$journal}</div>
-<a href="/">Back to today's journal</a>
+<div class="pt-2" data-testid="journal">
+  <h2 class="text-xl">
+    {date.toLocaleDateString(undefined, {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    })}
+  </h2>
+  <div class="pt-2 italic" data-testid="journal">{$journal}</div>
+</div>
